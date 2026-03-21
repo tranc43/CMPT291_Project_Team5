768ebac67902593159a443209367d0821139da5c
tranc43 <tranc43@mymacewan.ca>
2026-03-20 22:49:18 -0600

    Connection to SQL

--- MovieRental_Team5/MovieRental_Team5/Form1.cs
+++ MovieRental_Team5/MovieRental_Team5/Form1.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
 namespace MovieRental_Team5
 {
     public partial class Form1 : Form
     {
+        string connectionString = @"Server=ServerName;Database=MovieRental_Team5;Trusted_Connection=yes;";
+
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +15,18 @@ namespace MovieRental_Team5
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // comment test  
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    MessageBox.Show("Database connection successful!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Database connection failed: " + ex.Message);
+            }
         }
     }
 }
