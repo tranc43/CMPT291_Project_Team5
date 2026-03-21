d144635a73616b45b60fe8cb24fd4ce843d5448f
tranc43 <tranc43@mymacewan.ca>
2026-03-20 22:05:22 -0600

    repo test

---
+++ Team5_MovieRental/Team5_MovieRental_Project/Form1.cs
@@ -0,0 +1,10 @@
+namespace Team5_MovieRental_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Team5_Project/Form1.cs
@@ -0,0 +1,10 @@
+namespace Team5_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
