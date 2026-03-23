9ac75d4880915a05a5eab3f9bb5753e833cab2ed
tranc43 <tranc43@mymacewan.ca>
2026-03-22 21:47:48 -0600

    LoginForm Rework

--- MovieRental_Team5/MovieRental_Team5/LoginForm.cs
+++ MovieRental_Team5/MovieRental_Team5/LoginForm.cs
@@ -6,13 +6,13 @@ namespace MovieRental_Team5
 {
     public partial class LoginForm : Form
     {
-        string connectionString = @"Server=DESKTOP-CHLK2FI;Database=MovieRental_Team5;Trusted_Connection=yes;";
+        string connectionString = @"Server=DESKTOP-CHLK2FI\SQLEXPRESS;Database=MovieRental_Team5;Trusted_Connection=yes;";
 
         public LoginForm()
         {
             InitializeComponent();
         }
-
+        // this is making sure if the sql connection succeeds. If it does, it will show a message box saying "Database connection successful!" If it fails, it will show a message box with the error message.
         private void Form1_Load(object sender, EventArgs e)
         {
             try
@@ -28,30 +28,80 @@ namespace MovieRental_Team5
                 MessageBox.Show("Database connection failed: " + ex.Message);
             }
         }
-
-        private void label1_Click(object sender, EventArgs e)
+        private void LoginButton_Click(object sender, EventArgs e)
         {
+            string email = EmailField.Text.Trim();
 
-        }
+            if (string.IsNullOrEmpty(email))
+            {
+                MessageBox.Show("Error", "Please enter your email :(");
+                return;
+            }
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    // Employee Reader
+                    string employee_query = "SELECT Employee_ID, First_Name FROM Employee_Data WHERE SIN = @input";
+                    SqlCommand empCmd = new SqlCommand(employee_query, conn);
 
-        private void label1_Click_1(object sender, EventArgs e)
-        {
+                    empCmd.Parameters.AddWithValue("@input", email);
+                    SqlDataReader empReader = empCmd.ExecuteReader();
 
-        }
+                    if (empReader.HasRows)
+                    {
+                        empReader.Read();
+                        string employee_name = empReader["First_Name"].ToString();
+                        empReader.Close();
+                        MessageBox.Show("Login Successful!" + "Welcome Back," + Name + "!");
+                        return;
 
-        private void label2_Click(object sender, EventArgs e)
-        {
+                    }
+                    empReader.Close();
 
-        }
+                    // Customer check
+                    string customer_query = "SELECT Customer_ID, First_Name FROM Employee_Data WHERE Email_Address = @input";
+                    SqlCommand customer_cmd = new SqlCommand(customer_query, conn);
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+                    customer_cmd.Parameters.AddWithValue("@input", email);
+                    SqlDataReader customer_reader = empCmd.ExecuteReader();
 
+                    if (customer_reader.HasRows)
+                    {
+                        customer_reader.Read();
+                        string customer_name = customer_reader["First_Name"].ToString();
+                        customer_reader.Close();
+                        MessageBox.Show("Login Successful!" + "Welcome Back," + Name + "!");
+                        return;
+
+                    }
+                    customer_reader.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message + "there is an error!");
+            }
         }
 
         private void EmailAddressInput(object sender, EventArgs e)
         {
 
         }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_Application_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+   
     }
 }
