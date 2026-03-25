48370a4fc99259643c345384949bfe6c11d236db
tranc43 <tranc43@mymacewan.ca>
2026-03-24 22:10:11 -0600

    movie form implementation

--- MovieRental_Team5/MovieRental_Team5/MovieForm.Designer.cs
+++ MovieRental_Team5/MovieRental_Team5/MovieForm.Designer.cs
@@ -30,19 +30,28 @@
         {
             movie_dashboardd_title = new Label();
             genre_dropdown = new ComboBox();
-            genre_search = new TextBox();
+            title_search = new TextBox();
             input_movie_title = new Label();
             fee_field = new TextBox();
             distribution_fee = new Label();
             copies_label = new Label();
             num_copies = new TextBox();
+            load_movies = new Button();
+            controls = new Label();
+            add_movie = new Button();
+            update_movie = new Button();
+            delete_movie = new Button();
+            clear_button = new Button();
+            back_button = new Button();
+            movie_grid = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)movie_grid).BeginInit();
             SuspendLayout();
             // 
             // movie_dashboardd_title
             // 
             movie_dashboardd_title.AutoSize = true;
             movie_dashboardd_title.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
-            movie_dashboardd_title.Location = new Point(273, 24);
+            movie_dashboardd_title.Location = new Point(268, 9);
             movie_dashboardd_title.Name = "movie_dashboardd_title";
             movie_dashboardd_title.Size = new Size(245, 37);
             movie_dashboardd_title.TabIndex = 0;
@@ -53,33 +62,33 @@
             genre_dropdown.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             genre_dropdown.FormattingEnabled = true;
             genre_dropdown.Items.AddRange(new object[] { "Comedy", "Action", "Drama", "Foreign" });
-            genre_dropdown.Location = new Point(34, 134);
+            genre_dropdown.Location = new Point(351, 112);
             genre_dropdown.Name = "genre_dropdown";
             genre_dropdown.Size = new Size(121, 23);
             genre_dropdown.TabIndex = 1;
             genre_dropdown.Text = "Select Genre";
             // 
-            // genre_search
+            // title_search
             // 
-            genre_search.Location = new Point(175, 134);
-            genre_search.Name = "genre_search";
-            genre_search.PlaceholderText = "Enter Movie Name..";
-            genre_search.Size = new Size(124, 23);
-            genre_search.TabIndex = 2;
+            title_search.Location = new Point(492, 112);
+            title_search.Name = "title_search";
+            title_search.PlaceholderText = "Enter Movie Name..";
+            title_search.Size = new Size(124, 23);
+            title_search.TabIndex = 2;
             // 
             // input_movie_title
             // 
             input_movie_title.AutoSize = true;
             input_movie_title.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
-            input_movie_title.Location = new Point(103, 76);
+            input_movie_title.Location = new Point(45, 63);
             input_movie_title.Name = "input_movie_title";
-            input_movie_title.Size = new Size(196, 37);
+            input_movie_title.Size = new Size(150, 37);
             input_movie_title.TabIndex = 3;
-            input_movie_title.Text = "Input a Movie";
+            input_movie_title.Text = "Movie List";
             // 
             // fee_field
             // 
-            fee_field.Location = new Point(175, 203);
+            fee_field.Location = new Point(492, 184);
             fee_field.Name = "fee_field";
             fee_field.PlaceholderText = "Enter fee cost";
             fee_field.Size = new Size(124, 23);
@@ -89,7 +98,7 @@
             // 
             distribution_fee.AutoSize = true;
             distribution_fee.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            distribution_fee.Location = new Point(21, 201);
+            distribution_fee.Location = new Point(338, 182);
             distribution_fee.Name = "distribution_fee";
             distribution_fee.Size = new Size(134, 21);
             distribution_fee.TabIndex = 5;
@@ -99,7 +108,7 @@
             // 
             copies_label.AutoSize = true;
             copies_label.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            copies_label.Location = new Point(12, 244);
+            copies_label.Location = new Point(325, 219);
             copies_label.Name = "copies_label";
             copies_label.Size = new Size(147, 21);
             copies_label.TabIndex = 6;
@@ -107,27 +116,109 @@
             // 
             // num_copies
             // 
-            num_copies.Location = new Point(175, 244);
+            num_copies.Location = new Point(492, 221);
             num_copies.Name = "num_copies";
             num_copies.PlaceholderText = "Enter num of copies";
             num_copies.Size = new Size(124, 23);
             num_copies.TabIndex = 7;
+            // 
+            // load_movies
+            // 
+            load_movies.Location = new Point(366, 319);
+            load_movies.Name = "load_movies";
+            load_movies.Size = new Size(106, 38);
+            load_movies.TabIndex = 8;
+            load_movies.Text = "Load Movies";
+            load_movies.UseVisualStyleBackColor = true;
+            // 
+            // controls
+            // 
+            controls.AutoSize = true;
+            controls.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            controls.Location = new Point(544, 63);
+            controls.Name = "controls";
+            controls.Size = new Size(122, 37);
+            controls.TabIndex = 15;
+            controls.Text = "controls";
+            // 
+            // add_movie
+            // 
+            add_movie.Location = new Point(366, 266);
+            add_movie.Name = "add_movie";
+            add_movie.Size = new Size(106, 38);
+            add_movie.TabIndex = 16;
+            add_movie.Text = "Add Movie";
+            add_movie.UseVisualStyleBackColor = true;
+            // 
+            // update_movie
+            // 
+            update_movie.Location = new Point(492, 266);
+            update_movie.Name = "update_movie";
+            update_movie.Size = new Size(106, 38);
+            update_movie.TabIndex = 17;
+            update_movie.Text = "Update Movie";
+            update_movie.UseVisualStyleBackColor = true;
+            // 
+            // delete_movie
+            // 
+            delete_movie.Location = new Point(492, 319);
+            delete_movie.Name = "delete_movie";
+            delete_movie.Size = new Size(106, 38);
+            delete_movie.TabIndex = 18;
+            delete_movie.Text = "Delete Movie";
+            delete_movie.UseVisualStyleBackColor = true;
+            // 
+            // clear_button
+            // 
+            clear_button.Location = new Point(650, 206);
+            clear_button.Name = "clear_button";
+            clear_button.Size = new Size(106, 38);
+            clear_button.TabIndex = 19;
+            clear_button.Text = "Clear Fields";
+            clear_button.UseVisualStyleBackColor = true;
+            // 
+            // back_button
+            // 
+            back_button.Location = new Point(671, 400);
+            back_button.Name = "back_button";
+            back_button.Size = new Size(106, 38);
+            back_button.TabIndex = 21;
+            back_button.Text = "Back";
+            back_button.UseVisualStyleBackColor = true;
+            // 
+            // movie_grid
+            // 
+            movie_grid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            movie_grid.Location = new Point(33, 112);
+            movie_grid.Name = "movie_grid";
+            movie_grid.Size = new Size(162, 245);
+            movie_grid.TabIndex = 22;
             // 
             // MovieForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(movie_grid);
+            Controls.Add(back_button);
+            Controls.Add(clear_button);
+            Controls.Add(delete_movie);
+            Controls.Add(update_movie);
+            Controls.Add(add_movie);
+            Controls.Add(controls);
+            Controls.Add(load_movies);
             Controls.Add(num_copies);
             Controls.Add(copies_label);
             Controls.Add(distribution_fee);
             Controls.Add(fee_field);
             Controls.Add(input_movie_title);
-            Controls.Add(genre_search);
+            Controls.Add(title_search);
             Controls.Add(genre_dropdown);
             Controls.Add(movie_dashboardd_title);
             Name = "MovieForm";
             Text = "MovieForm";
+            Load += MovieForm_Load;
+            ((System.ComponentModel.ISupportInitialize)movie_grid).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -136,11 +227,19 @@
 
         private Label movie_dashboardd_title;
         private ComboBox genre_dropdown;
-        private TextBox genre_search;
+        private TextBox title_search;
         private Label input_movie_title;
         private TextBox fee_field;
         private Label distribution_fee;
         private Label copies_label;
         private TextBox num_copies;
+        private Button load_movies;
+        private Label controls;
+        private Button add_movie;
+        private Button update_movie;
+        private Button delete_movie;
+        private Button clear_button;
+        private Button back_button;
+        private DataGridView movie_grid;
     }
 }
--- MovieRental_Team5/MovieRental_Team5/MovieForm.cs
+++ MovieRental_Team5/MovieRental_Team5/MovieForm.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Forms;
 
@@ -10,9 +12,38 @@ namespace MovieRental_Team5
 {
     public partial class MovieForm : Form
     {
+        string connection = @"Server=DESKTOP-CHLK2FI\SQLEXPRESS;Database=MovieRental_Team5;Trusted_Connection=yes;";
+        private int selectedMovieID = -1; // for tracking movies editing/deleting
         public MovieForm()
         {
             InitializeComponent();
+        }
+
+        private void MovieForm_Load(object sender, EventArgs e)
+        {
+            genre_dropdown.Items.Clear();
+            genre_dropdown.Items.AddRange(new string[] { " Action", "Comedy", "Drama", "Foreign" });
+            genre_dropdown.SelectedIndex = 0;
+            load_movies();
+        }
+        private void load_movies()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connection))
+                {
+                    connection.Open();
+                    string movie_query = "SELECT Movie_ID, Movie_Name, Movie_Genre, Distribution_Fee, Num_Copies, Average_Rating FROM Movie_Data";
+                    SqlDataAdapter adapter = new SqlDataAdapter(movie_query, connection);
+                    DataTable data_t = new DataTable();
+                    adapter.Fill(data_t);
+                    movie_grid.DataSource = data_t;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
